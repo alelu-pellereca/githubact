8ead1f182eab60503f03947e2c15a23bbef23294
Alejandra Lucía Pellegrini Recabarren <alejandrapellegrini@gmail.com>
2024-10-30 21:51:12 +0100

    probando

--- src/app.js
+++ src/app.js
@@ -6,17 +6,22 @@ app.use(express.json());
 app.use(cors())
 
 // Ruta básica
-// Frontend
-app.get('/frontend', (req, res) => {
-    res.json({ image: 'alepellereca/imagenfront:2', description: 'Frontend' });
+app.get('/frontend', async (req, res) => {
+    const response = await fetch('http://frontend:3001')
+    const responseAux = await response.json()
+    res.status(200).json({ message: responseAux.message });
 });
-// Backend
-app.get('/backend', (req, res) => {
-    res.json({ image: 'alepellereca/imagenback:2', description: 'Backend' });
+app.get('/backend', async (req, res) => {
+    const response = await fetch('http://backend:8081')
+    const responseAux = await response.json()
+    res.status(200).json({ message: responseAux.message });
 });
+
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello World!' });
 });
+
+// Ruta de ejemplo para pruebas
 app.post('/echo', (req, res) => {
     const { body } = req;
     res.status(200).json(body);
